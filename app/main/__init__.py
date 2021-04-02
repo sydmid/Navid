ea06b8147df890d22d8d4036afd41e42145fc2ff
veteranomid <seyedomidnajmi@live.com>
2021-04-02 14:13:18 +0430

    some essential flask-jwt-extended callbacks and responses implemented

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_bootstrap import Bootstrap
@@ -35,6 +35,45 @@ def create_app(config_name):
         if identity == 1:  # Should be read from a config file or data base instead of hard coding
             return {'isAdmin': True}
         return {'isAdmin': False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback():
+        return jsonify({
+            'description': 'The token has expired',
+            'error': 'token_expired'
+        }), 401
+
+    # when the token they send us in the Authorization Header is an Actual JWT (I.E its some random number)
+    @jwt.invalid_token_loader
+    def expired_token_callback(error):
+        return jsonify({
+            'description': 'Signature verification failed',
+            'error': 'invalid_token'
+        }), 401
+
+    # when they don't send us a JWT at all
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({
+            'description': 'Request does not contain an access token',
+            'error': 'authorization_required'
+        }), 401
+
+    # when they send us a non-fresh token but our endpoint requires fresh token (l.E our item post)
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback():
+        return jsonify({
+            'description': 'The token is not fresh',
+            'error': 'fresh_token_required'
+        }), 401
+
+    # when the sign out and we don't want them to be able to use their last valid token anymore
+    @jwt.revoked_token_loader
+    def revoked_token_callback():
+        return jsonify({
+            'description': 'The token has been revoked',
+            'error': 'token_revoked'
+        }), 401
 
     @app.before_first_request
     def create_tables():
