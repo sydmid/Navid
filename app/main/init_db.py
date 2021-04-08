7d29ed9cb12e7e153637760e0db5314c9fdd9e6b
veteranomid <seyedomidnajmi@live.com>
2021-04-08 17:07:14 +0430

    init database with vanilla sqlalchemy

---
+++ app/main/init_db.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from sqlalchemy import (MetaData, Table, Column, Integer, Float, Numeric, String, DateTime, ForeignKey, create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from tset_client import Downloader
+import pandas as pd
+
+Base = declarative_base()
+engine = create_engine('sqlite:///test.db')
+
+
+metadata = MetaData()
+downloader = Downloader()
+downloaded = downloader.update()
+
+for key in downloaded.keys():
+    newTable = type("Record", (Base,), {
+        '__tablename__': f"{key}",
+        'date': Column(DateTime, primary_key=True),
+        'open': Column(Float),
+        'high': Column(Float),
+        'low': Column(Float),
+        'adjClose': Column(Float),
+        'value': Column(Float),
+        'volume': Column(Float),
+        'count': Column(Float),
+        'close': Column(Float),
+    })
+
+Base.metadata.create_all(engine)
